3613201bd62002c255dc90bbc3176da5f4d36b5e
cristianmilo07 <cristianmilo07@gmail.com>
2022-03-18 20:58:56 -0500

    Primera parte del desarrollo de la aplicación

---
+++ lib/review_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'review.dart';
+
+class ReviewList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Review("assets/img/people.jpg", "Cristian Villada", "1 review · 5 photos", "There is an amazing place in Sri Lanka"),
+        Review("assets/img/girl.jpg", "Carolina Rúa", "2 review · 5 photos", "Hijo que le debes un percimo a cristian villada"),
+        Review("assets/img/ann.jpg", "Gissele Thomas", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+
+      ],
+    );
+  }
+
+}
